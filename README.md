76e5859e9c7aace063be82755f0f11cec380a592
Ogun Isik <ogunisik@gmail.com>
2019-02-21 17:49:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# sozluk99
+Bu proje blogspot üzerinde yer alan [sözlük99](https://eksisozluk.com/sozluk99--1489289) arşivini daha kullanılabilir bir hale getirebilmek için oluşturuldu. Alınan arşivin çeşitli formatlarına ve json dosyasını oluşturan koda aşağıdan ulaşabilirsiniz:
+- [sozluk99.rar](sozluk99.rar)
+- [sozluk99.json](sozluk99.json)
+- [sozluk99.csv](sozluk99.csv)
+- [sozluk99-mongodb-4.0.6.archive](sozluk99-mongodb-4.0.6.archive)
+- [parse.py](parser/parse.py)
+
+> [ciemori](https://eksisozluk.com/ciemori--1084980) ve [robin](https://eksisozluk.com/robin--35674)'in öncülüğünde yazarların katkılarıyla oluşturulmuş, sözlükte 99 ruhunu ölümsüz kılmayı amaçlayan site. elinize sağlık.
+> 
+> http://sozluk99.blogspot.com/
+>
+> [16.02.2006 00:10 ~ 18:57](https://eksisozluk.com/entry/9133095), [sedge](https://eksisozluk.com/biri/sedge)
+
+
+
+## olay ne?
+[ek\$i sozluk yedinci yil nostalji sekansi](https://eksisozluk.com/eksi-sozluk-yedinci-yil-nostalji-sekansi--1489276) hasebiyle alinan capture, html kayitlari dahil bilumum dokumani ortakla\$a bir adreste toplamak maksadiyla actik bu blog'u. 14/15.02.2006 tarihinde ya\$anan bu doga olayini unutmamak ve capture ar$ivinin eksik parcalarini tamamlamak isteyenler icin bir yedinci ya\$ anisi. [elindeki capture'lari yollamayan michael jackson olsun] sevgi!
+
+## ek\$i sozluk backup \$eysi
+toplamda yaklaşık 20900 adet dosya bulunacak zipin içinde. zipi açtığınzda yaklaşık 150mbyte'a çıkacaktır endişelenmeyin. açtıktan sonra zip içeriğini extract ederken sürükle-bırak yapmak yerine "extract to" üzerinden dışarı alarak efendi bir gidiş sergileyin, mutlu olacaksınız...
+
+bu backupları daha da düzgün almak isterdim ancak bağlantımızın kısıtlılığı nedeniyle, istediğim gibi olmadı. 99 yılında girilmiş olup da 15 şubat 2006 günü karşımızdan geçit töreniyle geçen bazı başlık ve entryler karşınıza çıkmayabilir, şimdiden özür dilerim, tamamıyla kontrolüm dışında gelişmiştir ne geliştiyse. özellikle son dakikalarda girilen bazı entryleri kaçırmış olmak hiç hoş durmadı.
+
+zip içinde "index.asp@a=td&p=114.htm" gibi isimleri olanlar, "günün entryleri" listesinin sayfaları olmaktadır. onları çift tıklayıp açtığınızda karşınıza çıkan listedeki başlıklara tıklarsanız, yeni bir sayfada açılırlar.
+
+ancak en mantıklısı =>""default0.html""<= dosyasını açıp, normal bir şekilde sözlükte dolaşıyormuş gibi, offline vaziyette sözlük 99 içinde dolaşmaktır. ancak, özellikle "bugün" kısmının ilk 2-3 sayfasındaki linklerden çalışmayanlar olabilir, son dakikada yetişmemeleri ile ilişkili olacaktır. onun dışındaki tüm sayfaların sorunsuz olacağına inancımız sonsuzdur...
+
+nostalji güzeldir diyerek şeyimi şey yapıyorum, iyi eğlenceler... buyrunuz ikinci link (neden ilk link yok diyenlere: ilk link ham dosyalardan oluşuyordu, html içerikli olsun, kolay kullanılsın dedim, fena mı etmişim?):
+
+## contributors
+spinapubica
+ciémori
+kbertan
+r2
+jose arcadio buendia
+tabudeviren
+jamesjersey
+plainwalker
+robin
+error 6
+darkordek
+woodstock
+goklemez
+alp
+byron
+deepwonder
+wendy elizabeth
+ezeriko
+S:Renovation
+tenochtitlan
+cig borek
+luzumsuzadam
+quadropol
+erleichda
+tatli su kurnazi
---
+++ parser/parse.py
@@ -0,0 +1,119 @@
+""" sozluk99 iceriklerini parse eder """
+from datetime import datetime
+from os import listdir
+from os.path import join
+from urllib.parse import parse_qs, urlparse
+
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient, ReplaceOne
+
+CLIENT = MongoClient("localhost", 27017)
+MONGO_TOPICS = CLIENT.eksi.topics
+
+TOPICS_PATH = "data/"
+GEVENT_JOBS = []
+
+def get_topic_files():
+    """ basliklari getirir """
+    topics = (topic for topic in listdir(TOPICS_PATH) if topic.startswith("show.asp"))
+
+    return topics
+
+def get_content_from_file(path):
+    """ verilen pathin icerigini getirir """
+    file_path = join(TOPICS_PATH, path)
+    with open(file_path, mode="r", encoding="iso-8859-9") as file:  
+        return file.read() 
+
+def get_entry_tags(html_doc):
+    """ verilen html icerisindeki entryleri getirir """
+    soup = BeautifulSoup(html_doc, "html.parser")
+    return soup.select("ol#el li")
+
+def parse_entry_tag(entry_tag):
+    """ verilen html entry'i objeye cevirir """
+    id = entry_tag["id"].strip("d")
+
+    nick_tag = entry_tag.select_one("div.aul a")
+    nick = nick_tag.get_text()
+    nick_tag.decompose()
+
+    date_tag = entry_tag.select_one("div.aul")
+    date_str = date_tag.get_text()
+    date_str = date_str.split("~")[0]
+    date_str = date_str.strip(" (),")
+    try:
+        date = datetime.strptime(date_str, "%d.%m.%Y")
+    except:
+        date = datetime.strptime(date_str, "%d.%m.%Y %H:%M")
+
+    date_tag.decompose()
+
+    entry_tag.select_one("div").decompose()
+
+    text = entry_tag.get_text()
+
+    return {"id": id, "nick": nick, "date": date, "text": text}
+
+def get_title_from_file(file_name):
+    """ dosya isminden topic adini al """
+    file_name = file_name.replace("@", "?").replace(".htm", "")
+    qs_list = parse_qs(urlparse(file_name).query)
+    
+    if "t" not in qs_list:
+        return ""
+    
+    file_name = qs_list["t"][0]    
+    
+    return file_name
+
+def insert_documents(counter, requests, last_insert=False):
+    """ bulk insert  """
+    global GEVENT_JOBS
+
+    if counter > 1000 or last_insert:
+        GEVENT_JOBS.append(gevent.spawn(MONGO_TOPICS.bulk_write, requests.copy(), False))
+        requests.clear()
+        return 0
+
+    return counter + 1
+
+def parse():
+    """ donusum islemini gerceklestirir """
+    global GEVENT_JOBS
+
+    topic_files = get_topic_files()
+
+    requests = []
+
+    counter = 0
+
+    for topic_file in topic_files:
+        topic_html = get_content_from_file(topic_file)
+        entry_tags = get_entry_tags(topic_html)
+
+        title = get_title_from_file(topic_file)
+
+        if not entry_tags or not title:
+            continue
+
+        for entry_tag in entry_tags:
+            entry = parse_entry_tag(entry_tag)
+            entry["title"] = title
+
+            requests.append(ReplaceOne({"id": entry["id"]}, entry, upsert=True))
+            counter = insert_documents(counter, requests)
+            
+
+    insert_documents(counter, requests, True)
+    gevent.joinall(GEVENT_JOBS)
+
+if __name__ == "__main__":
+    print(datetime.now())
+    parse()
+    print(datetime.now())
+    print("finished!")
